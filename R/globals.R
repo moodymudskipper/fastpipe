efc5a0423ff53696bc8eb2d8c883e060a682f0da
moodymudskipper <antoine.fabri@gmail.com>
2019-10-08 00:33:30 +0200

    remove more obsolete comments

--- R/globals.R
+++ R/globals.R
@@ -1,16 +1,8 @@
 
 globals <- new.env()
-#globals$master <- TRUE
-#master_on  <- function() globals$master <- TRUE
 master_off <- function() globals$master <- FALSE
-
-#globals$is_fs <- FALSE
 fs_on  <- function() globals$is_fs <- TRUE
-#fs_off <- function() globals$is_fs <- FALSE
-
-#globals$is_compound <- FALSE
 compound_on  <- function() globals$is_compound <- TRUE
-#compound_off <- function() globals$is_compound <- FALSE
 
 reset_globals <- function(){
   globals[["master"]] <- TRUE
